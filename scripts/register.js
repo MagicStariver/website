425d35996d5373997f2620e98f6bad1f46a20d34
MagicStariver <kychoy1216@gmail.com>
2024-08-13 16:44:51 +0800

    download and add firebase

--- scripts/register.js
+++ scripts/register.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHW8gPuNSVstSV0ytE8oB5-_3PJKvxgMA",
+  authDomain: "muzica-93e9c.firebaseapp.com",
+  projectId: "muzica-93e9c",
+  storageBucket: "muzica-93e9c.appspot.com",
+  messagingSenderId: "559137569600",
+  appId: "1:559137569600:web:081ec42350a9f8099658a5",
+  measurementId: "G-G5MCSMD8H0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 document.addEventListener('DOMContentLoaded', function() {
     const registerForm = document.getElementById('registerForm');
     const registerMessage = document.getElementById('registerMessage');
