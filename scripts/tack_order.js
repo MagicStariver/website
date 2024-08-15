91da8d98e67ebba4eee9670d2c175f3eb7f195aa
Yang88l <96825030+Yang88l@users.noreply.github.com>
2024-08-15 09:48:58 +0800

    making arrow for each order status

--- scripts/tack_order.js
+++ scripts/tack_order.js
@@ -4,13 +4,46 @@ packing = document.getElementById("packing");
 delivery = document.getElementById("delivery");
 arrived = document.getElementById("arrived");
 
-isDone=true;
+order_status="arrived";
 
-if(isDone==true){
+
+
+
+if (order_status=="arrived"){
     payment.style="border: 2px solid #130505;" //solid line
+    packing.style="border: 2px solid #130505;" //solid line
+    delivery.style="border: 2px solid #130505;" //solid line
+    arrived.style="border: 2px solid #130505;" //solid line
+    document.getElementById("loader1").classList.add("stop-animation");
+    document.getElementById("loader2").classList.add("stop-animation");
+    document.getElementById("loader3").classList.add("stop-animation");
 }
-else{
-    payment.style="border: 2px dashed #130505;" //dashed line
+else if (order_status=="delivery"){
+    payment.style="border: 2px solid #130505;" //solid line
+    packing.style="border: 2px solid #130505;" //solid line
+    delivery.style="border: 2px solid #130505;" //solid line
+    arrived.style="border: 2px dashed #130505;" //dashed line
+    document.getElementById("loader1").classList.add("stop-animation");
+    document.getElementById("loader2").classList.add("stop-animation");
+    document.getElementById("loader3").classList.remove("stop-animation");
+}
+else if (order_status=="packing"){
+    payment.style="border: 2px solid #130505;" //solid line
+    packing.style="border: 2px solid #130505;" //solid line
+    delivery.style="border: 2px dashed #130505;" //dashed line
+    arrived.style="border: 2px dashed #130505;" //dashed line
+    document.getElementById("loader1").classList.add("stop-animation");
+    document.getElementById("loader2").classList.remove("stop-animation");
+    document.getElementById("loader3").classList.remove("stop-animation");
+}
+else if (order_status=="payment"){
+    payment.style="border: 2px solid #130505;" //solid line
+    packing.style="border: 2px dashed #130505;" //dashed line
+    delivery.style="border: 2px dashed #130505;" //dashed line
+    arrived.style="border: 2px dashed #130505;" //dashed line
+    document.getElementById("loader1").classList.remove("stop-animation");
+    document.getElementById("loader2").classList.remove("stop-animation");
+    document.getElementById("loader3").classList.remove("stop-animation");
 }
 
-product_name.innerHTML="guitar"
+//product_name.innerHTML="guitar"
